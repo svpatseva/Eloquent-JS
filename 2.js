9e3f209481557ee6ebb1e7c9058b649aaf7ca891
svpatseva <svpatseva@yandex.ru>
2017-10-13 17:32:18 +0300

    1

--- 2.js
+++ 2.js
@@ -1,8 +1,16 @@
 for (var number = 1; number <= 100; number++) {
-    var result = "";
-  if (number % 3 == 0)
-    result += "Fizz"; 
+  if (number % 3 == 0 && number % 5 == 0)
+  console.log("FizzBuzz");
+    
+  else if (number % 3 == 0)
+    console.log("Fizz");
+
   else if (number % 5 == 0)
-    result += "Buzz"; 
-    console.log(result || number); 
+    console.log("Buzz");
+
+    else
+        console.log(number);
 }
+
+
+
--- 4.js
+++ 4.js
@@ -5,6 +5,8 @@ function min (a, b) {
         return b;
 }
 
+//return (a < b) ? a : b; то же самое
+
 console.log(min(0, 10));
 // → 0
 console.log(min(0, -10));
